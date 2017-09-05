70b5dd13895f832419eef9c75cf6f647e3a2bea4
Luo, Yuyang <yuyluo@paypal.com>
2017-09-06 00:29:26 +0800

    modify scenekit demo project: add pikachu model into the scene

--- SceneKitDemo/SceneKitDemo/GameViewController.swift
+++ SceneKitDemo/SceneKitDemo/GameViewController.swift
@@ -31,28 +31,22 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(node)
         
         scnView.allowsCameraControl = true
-                // add a tap gesture recognizer
-                let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-                scnView.addGestureRecognizer(tapGesture)
+
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        scnView.addGestureRecognizer(tapGesture)
     }
     
     private func generateNode() -> SCNNode? {
-        guard let path = Bundle.main.path(forResource: "PikachuM_ColladaMax",
-                                          ofType: "dae") else {
-                                            return nil
-        }
-        
-        let asset = MDLAsset(url: URL(string: path)!)
-        let object = asset.object(at: 0)
-        let node = SCNNode(mdlObject: object)
-        
-        return node
+        return SCNScene(named: "untitled.dae")?.rootNode.childNode(withName: "PikachuM", recursively: true)
     }
     
     private func setupCameraNode() {
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
-        cameraNode.position = SCNVector3(x: 0, y: 1.6, z: 2.8)
+        cameraNode.position = SCNVector3(x: -6, y: 0, z: 18)
+        // pokemon face to negative x
+        // camera face to negative z, position at (0,0,0)
     }
     
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
@@ -113,66 +107,5 @@ class GameViewController: UIViewController {
 
 }
 
-
-
-
-
-
-
-
-
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // create and add a camera to the scene
-//        let cameraNode = SCNNode()
-//        cameraNode.camera = SCNCamera()
-//        scene.rootNode.addChildNode(cameraNode)
-//
-//        // place the camera
-//        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-//
-//        // create and add a light to the scene
-//        let lightNode = SCNNode()
-//        lightNode.light = SCNLight()
-//        lightNode.light!.type = .omni
-//        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-//        scene.rootNode.addChildNode(lightNode)
-//
-//        // create and add an ambient light to the scene
-//        let ambientLightNode = SCNNode()
-//        ambientLightNode.light = SCNLight()
-//        ambientLightNode.light!.type = .ambient
-//        ambientLightNode.light!.color = UIColor.darkGray
-//        scene.rootNode.addChildNode(ambientLightNode)
-//
-//        // retrieve the ship node
-//        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-//
-//        // animate the 3d object
-//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-//
-//        // retrieve the SCNView
-//        let scnView = self.view as! SCNView
-//
-//        // set the scene to the view
-//        scnView.scene = scene
-//
-//        // allows the user to manipulate the camera
-//        scnView.allowsCameraControl = true
-//
-//        // show statistics such as fps and timing information
-//        scnView.showsStatistics = true
-//
-//        // configure the view
-//        scnView.backgroundColor = UIColor.black
-//
-//        // add a tap gesture recognizer
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-//        scnView.addGestureRecognizer(tapGesture)
-//    }
 
 
